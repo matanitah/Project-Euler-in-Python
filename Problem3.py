edca9da2cb23b9da8b58f89836fed8fa6d4fffec
Matan Itah <matanitah212@gmail.com>
2021-12-30 11:25:05 -0800

    Solved problems 2 and 3.

---
+++ Problem3.py
@@ -0,0 +1,14 @@
+import math
+
+n = 600851475143
+max=0
+
+for i in range(3,int(math.sqrt(n))):
+
+        # while i divides n , print i and divide n
+    while n % i== 0:
+        if(i>max):
+            max=i
+        n = n / i
+
+print(max)
