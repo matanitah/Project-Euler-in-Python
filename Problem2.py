4b5fd083089bfe8e47601c17e67e1b76aaa3c6db
Matan Itah <matanitah212@gmail.com>
2021-12-30 11:29:12 -0800

    Added comments with the problem in question.

--- Problem2.py
+++ Problem2.py
@@ -1,3 +1,6 @@
+"""
+By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+"""
 def fibb(n):
     if(n==0):
         return 1
--- Problem3.py
+++ Problem3.py
@@ -1,3 +1,6 @@
+"""
+What is the largest prime factor of the number 600851475143 ?
+"""
 import math
 
 n = 600851475143
